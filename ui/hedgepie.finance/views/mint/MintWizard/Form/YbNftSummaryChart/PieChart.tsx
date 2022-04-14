42727d753cbb7defcb5a84b78294d5ecde2bd363
shamrockys <todo1017@outlook.com>
2022-04-15 01:21:02 +0800

    disable chart

--- ui/hedgepie.finance/views/mint/MintWizard/Form/YbNftSummaryChart/PieChart.tsx
+++ ui/hedgepie.finance/views/mint/MintWizard/Form/YbNftSummaryChart/PieChart.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box } from 'theme-ui'
-import * as d3 from 'd3'
+// import * as d3 from 'd3'
 import MintWizardContext from 'contexts/MintWizardContext'
 
 const PieChart = () => {
@@ -10,35 +10,35 @@ const PieChart = () => {
 
   const drawChart = () => {
 
-    // dimension
-    const width = rootRef.current.clientWidth
-    const radius = width / 2
+    // // dimension
+    // const width = rootRef.current.clientWidth
+    // const radius = width / 2
 
-    // data
-    const allocated = Math.min(100, formData.allocated)
-    const values = [allocated, 100 - allocated]
-    const indexes = d3.range(values.length)
-    const colors = ['#C92144', '#A11D2B']
+    // // data
+    // const allocated = Math.min(100, formData.allocated)
+    // const values = [allocated, 100 - allocated]
+    // const indexes = d3.range(values.length)
+    // const colors = ['#C92144', '#A11D2B']
 
-    // arc
-    const arcs = d3.pie().padAngle(0).sort(null).value((i: any) => values[i])(indexes)
-    const arc: any = d3.arc().innerRadius(0).outerRadius(radius)
+    // // arc
+    // const arcs = d3.pie().padAngle(0).sort(null).value((i: any) => values[i])(indexes)
+    // const arc: any = d3.arc().innerRadius(0).outerRadius(radius)
 
-    // svg
-    const svg = d3.select(rootRef.current)
-      .attr('width', width)
-      .attr('height', width)
-      .attr('viewBox', [-width / 2, -width / 2, width, width])
+    // // svg
+    // const svg = d3.select(rootRef.current)
+    //   .attr('width', width)
+    //   .attr('height', width)
+    //   .attr('viewBox', [-width / 2, -width / 2, width, width])
 
-    svg.selectAll('g').remove()
+    // svg.selectAll('g').remove()
 
-    svg.append('g')
-      .selectAll('path')
-      .attr('stroke-width', 0)
-      .data(arcs)
-      .join('path')
-      .attr('fill', (d: any) => colors[d.data])
-      .attr('d', arc)
+    // svg.append('g')
+    //   .selectAll('path')
+    //   .attr('stroke-width', 0)
+    //   .data(arcs)
+    //   .join('path')
+    //   .attr('fill', (d: any) => colors[d.data])
+    //   .attr('d', arc)
   }
 
   React.useEffect(() => {
